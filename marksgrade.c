af5b99a3f2a758e4049cbdd0eae41486592c9079
saibrahmani-ux <saibrahmanidamaraju@gmail.com>
2025-09-15 21:34:42 +0530

    Update marksgrade.c

--- marksgrade.c
+++ marksgrade.c
@@ -1,5 +1,13 @@
+//max marks=40
 #include<stdio.h>
 int main(){
-  printf("hello world");
+ int arr[100],n;
+  printf("no of students in class");
+  scanf("%d",&n);
+  for(int i=0;i<n;i++){
+    scanf("%d",&arr[i];
+    
+  }
+  
 return 0;
 }
